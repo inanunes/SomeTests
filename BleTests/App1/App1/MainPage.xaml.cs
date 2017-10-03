e07f85a2b64f21c8a56523992a7f6b9898c150bd
Inhayan Nunes <iwsn@cin.ufpe.br>
2017-10-03 16:30:57 -0300

    creating folder by employee and adding "ponto" on output paht.

--- BleTests/App1/App1/MainPage.xaml.cs
+++ BleTests/App1/App1/MainPage.xaml.cs
@@ -1,108 +1,200 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
+using System.Threading;
 using Windows.Devices.Bluetooth.Advertisement;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace App1
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    private const int DISTANCE_THRESHOLD = -45;
+    private const int CONNECTION_TIME_IN_SECONDS = 3;
+    private int count = 1;
+    private int ammount = 0;
+    private string msg;
+    private Color msgColor;
+    private DistanceState currentDistanceState = DistanceState.FAR;
+    private Timer timer;
+    private int seconds;
+    private DistanceState oldDistanceState;
+    private ConnectionState connectionState = ConnectionState.DISCONNECTED;
+    private ContentDialog dialog = new ContentDialog();
+
+
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-
-        private void btnDiscover_Click(object sender, RoutedEventArgs e)
-        {
-            BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
-            watcher.Received += OnAdvertisementReceived;
-            watcher.ScanningMode = BluetoothLEScanningMode.Active;
-
-
-            //// Set the in-range threshold to -70dBm. This means advertisements with RSSI >= -70dBm 
-            //// will start to be considered "in-range" (callbacks will start in this range).
-            //watcher.SignalStrengthFilter.InRangeThresholdInDBm = -70;
-
-            //// Set the out-of-range threshold to -75dBm (give some buffer). Used in conjunction 
-            //// with OutOfRangeTimeout to determine when an advertisement is no longer 
-            //// considered "in-range".
-            //watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -75;
-
-            //// Set the out-of-range timeout to be 2 seconds. Used in conjunction with 
-            //// OutOfRangeThresholdInDBm to determine when an advertisement is no longer 
-            //// considered "in-range"
-            //watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(2000);
-
-            watcher.Start();
-        }
-
-        private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
-        {
-            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-           () =>
-            {
-            //    IList<BluetoothLEManufacturerData> manufacturerDataList = args.Advertisement.ManufacturerData;
-
-            //    List< BluetoothLEManufacturerData> list = manufacturerDataList.ToList<BluetoothLEManufacturerData>();
-
-            //    txtDistance.Text = string.Format("Company ID: {0} , Data: {1} , Distance: {2}", list[0].CompanyId, list[0].Data, args.RawSignalStrengthInDBm);
-
-            txtDistance.Text = args.RawSignalStrengthInDBm.ToString();
-
-            }
-           );
-        }
-
-        private void btnAdvertise_Click(object sender, RoutedEventArgs e)
-        {
-            BluetoothLEAdvertisementPublisher publisher = new BluetoothLEAdvertisementPublisher();
-
-            // Add custom data to the advertisement
-            var manufacturerData = new BluetoothLEManufacturerData();
-            manufacturerData.CompanyId = 0xFFFE;
-
-            var writer = new DataWriter();
-            writer.WriteString("Hello World");
-
-            // Make sure that the buffer length can fit within an advertisement payload (~20 bytes). 
-            // Otherwise you will get an exception.
-            manufacturerData.Data = writer.DetachBuffer();
-
-            // Add the manufacturer data to the advertisement publisher:
-            publisher.Advertisement.ManufacturerData.Add(manufacturerData);
-            publisher.StatusChanged += Publisher_StatusChanged;
-
-            publisher.Start();
-
-        }
-
-        private void Publisher_StatusChanged(BluetoothLEAdvertisementPublisher sender, BluetoothLEAdvertisementPublisherStatusChangedEventArgs args)
-        {
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
-            {
-                txtStatus.Text = args.Status.ToString() + " " + args.Error.ToString();
-            }
-            );
-        }
+      this.InitializeComponent();
     }
+
+    private void btnDiscover_Click(object sender, RoutedEventArgs e)
+    {
+      BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
+      watcher.Received += OnAdvertisementReceived;
+      watcher.ScanningMode = BluetoothLEScanningMode.Active;
+      watcher.AdvertisementFilter.Advertisement.ServiceUuids.Add(Guid.Parse("CDB7950D-73F1-4D4D-8E47-C090502DBD63"));
+
+      //var manufacturerData = new BluetoothLEManufacturerData();
+      //manufacturerData.CompanyId = 0x0075;
+
+      //// Make sure that the buffer length can fit within an advertisement payload (~20 bytes). 
+      //// Otherwise you will get an exception.
+      //var writer = new DataWriter();
+      //writer.WriteString("Data");
+      //manufacturerData.Data = writer.DetachBuffer();
+      //watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
+
+      //// Set the in-range threshold to -70dBm. This means advertisements with RSSI >= -70dBm 
+      //// will start to be considered "in-range" (callbacks will start in this range).
+      //watcher.SignalStrengthFilter.InRangeThresholdInDBm = -70;
+
+      //// Set the out-of-range threshold to -75dBm (give some buffer). Used in conjunction 
+      //// with OutOfRangeTimeout to determine when an advertisement is no longer 
+      //// considered "in-range".
+      //watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -75;
+
+      //// Set the out-of-range timeout to be 2 seconds. Used in conjunction with 
+      //// OutOfRangeThresholdInDBm to determine when an advertisement is no longer 
+      //// considered "in-range"
+      //watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(2000);
+
+      watcher.Start();
+      StartConnectionTimer();
+    }
+
+    private void StartConnectionTimer()
+    {
+      timer = new Timer((e) => { NotifyConnection(); }, null, 0, 1000);
+    }
+
+    private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+    {
+      await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+     () =>
+      {
+
+        txtSignal.Text = string.Format("[{0}]", args.RawSignalStrengthInDBm.ToString()); 
+
+        count++;
+        ammount += args.RawSignalStrengthInDBm;
+                
+        if (count == 5)
+        {
+          int rssiAVG = ammount / count;
+
+          if (rssiAVG < DISTANCE_THRESHOLD)
+          {
+            msg = "It's far!!!";
+            msgColor = Color.FromArgb(255, 255, 0, 0);
+            currentDistanceState = DistanceState.FAR;
+          }
+          else
+          {
+            msg = "It's close!!!";
+            msgColor = Color.FromArgb(255, 0, 255, 0);
+            currentDistanceState = DistanceState.CLOSE;
+          }
+
+          txtDistance.Foreground = new SolidColorBrush(msgColor);
+          txtDistance.Text = msg;
+
+          count = 0;
+          ammount = 0;
+        }
+      }
+     );
+    }
+
+    private void btnAdvertise_Click(object sender, RoutedEventArgs e)
+    {
+      BluetoothLEAdvertisementPublisher publisher = new BluetoothLEAdvertisementPublisher();
+
+      // Add custom data to the advertisement
+      var manufacturerData = new BluetoothLEManufacturerData();
+      manufacturerData.CompanyId = 0xFFFE;
+
+      var writer = new DataWriter();
+      writer.WriteString("Hello World");
+
+      // Make sure that the buffer length can fit within an advertisement payload (~20 bytes). 
+      // Otherwise you will get an exception.
+      manufacturerData.Data = writer.DetachBuffer();
+
+      // Add the manufacturer data to the advertisement publisher:
+      publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+      publisher.StatusChanged += Publisher_StatusChanged;
+
+      publisher.Start();
+
+    }
+
+    private async void Publisher_StatusChanged(BluetoothLEAdvertisementPublisher sender, BluetoothLEAdvertisementPublisherStatusChangedEventArgs args)
+    {
+      await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+      () =>
+      {
+        txtStatus.Text = args.Status.ToString() + " " + args.Error.ToString();
+      }
+      );
+    }
+
+    private async void NotifyConnection()
+    {
+      await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+      () =>
+      {
+        if (oldDistanceState == currentDistanceState)
+        {
+          seconds++;
+          if (seconds == CONNECTION_TIME_IN_SECONDS)
+          {
+            if (ConnectionState.DISCONNECTED.Equals(connectionState))
+            {
+              if (DistanceState.CLOSE.Equals(currentDistanceState))
+              {
+                connectionState = ConnectionState.CONNECTED;
+                txtConnectionState.Text = "Connected!!!";
+              }
+            }
+            else
+            {
+              if (DistanceState.FAR.Equals(currentDistanceState))
+              {
+                connectionState = ConnectionState.DISCONNECTED;
+                txtConnectionState.Text = "Disconnected!!!";
+              }
+            }
+            seconds = 0;
+          }
+        }
+        else
+        {
+          seconds = 0;
+        }
+        oldDistanceState = currentDistanceState;
+      });
+    }
+
+    enum DistanceState
+    {
+      CLOSE,
+      FAR
+    }
+
+    enum ConnectionState
+    {
+      CONNECTED,
+      DISCONNECTED
+    }
+  }
 }
--- SplitPDF/SplitPDF/MainWindow.xaml.cs
+++ SplitPDF/SplitPDF/MainWindow.xaml.cs
@@ -20,146 +20,144 @@ using System.Windows.Forms;
 
 namespace SplitPDF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-
-
-        private static void SplitPDF(string basePDFPath, string outputPath, bool isMonthly)
-        {
-            PdfReader reader = new PdfReader(basePDFPath);
-
-            FileInfo file = new FileInfo(basePDFPath);
-            string pdfFileName = file.Name.Substring(0, file.Name.LastIndexOf(".")) + " - ";
-
-            int pagesOnSplited = isMonthly ? 2 : 1;
-
-            for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber += pagesOnSplited)
-            {
-                string employeeName = ExtractEmployeeNameFromPDFPage(basePDFPath, pageNumber);
-                string newPdfFileName = string.Format(pdfFileName + "{0}", employeeName);
-
-                CreateAndSaveEmployeePDF(basePDFPath, outputPath, pageNumber, isMonthly, newPdfFileName);
-            }
-        }
-
-        private static string ExtractEmployeeNameFromPDFPage(string path, int pageNumber)
-        {
-            string pdfTxt = GetTextFromPDFPage(path, pageNumber);
-            string name = GetEmployeeNameOnText(pdfTxt);
-            return name;
-        }
-
-        private static string GetTextFromPDFPage(string path, int pageNumber)
-        {
-            string pdfTxt;
-
-            using (PdfReader reader = new PdfReader(path))
-            {
-                pdfTxt = PdfTextExtractor.GetTextFromPage(reader, pageNumber);
-            }
-
-
-            return pdfTxt;
-        }
-
-        private static string GetEmployeeNameOnText(string pdfTxt)
-        {
-            string baseStringLeft = "Funcionário:";
-            string baseStringRight = "Matrícula:";
-            int Start, End;
-
-            if (pdfTxt.Contains(baseStringLeft) && pdfTxt.Contains(baseStringRight))
-            {
-                Start = pdfTxt.IndexOf(baseStringLeft, 0) + baseStringLeft.Length;
-                End = pdfTxt.IndexOf(baseStringRight, Start);
-                return pdfTxt.Substring(Start, End - Start).Trim();
-            }
-            else
-            {
-                return "";
-            }
-        }
-
-        private static void CreateAndSaveEmployeePDF(string pdfFilePath, string outputPath, int startPage, bool isMonthly, string pdfFileName)
-        {
-            using (PdfReader reader = new PdfReader(pdfFilePath))
-            {
-                if (!Directory.Exists(outputPath))
-                {
-                    Directory.CreateDirectory(outputPath);
-                }
-
-                Document document = new Document();
-                PdfCopy copy = new PdfCopy(document, new FileStream(outputPath + "\\" + pdfFileName + ".pdf", FileMode.Create));
-                document.Open();
-
-                copy.AddPage(copy.GetImportedPage(reader, startPage));
-
-                if (isMonthly)
-                {
-                    copy.AddPage(copy.GetImportedPage(reader, startPage + 1));
-                }
-
-                document.Close();
-            }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            // Create OpenFileDialog 
-           OpenFileDialog dlg = new OpenFileDialog();
-            
-            // Set filter for file extension and default file extension 
-            dlg.DefaultExt = ".pdf";
-            dlg.Filter = "PDF Files (*.pdf)|*.pdf";
-
-
-            // Display OpenFileDialog by calling ShowDialog method 
-            DialogResult result = dlg.ShowDialog();
-            string filename = dlg.FileName;
-
-            // Get the selected file name and display in a TextBox 
-            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(filename))
-            {
-                txtBasePdf.Text = filename;
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            using (var fbd = new FolderBrowserDialog())
-            {
-                DialogResult result = fbd.ShowDialog();
-
-                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                {
-                    txtOutputPath.Text = fbd.SelectedPath;
-                }
-            }
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                SplitPDF(txtBasePdf.Text, txtOutputPath.Text, false);
-                elpReport.Fill = new SolidColorBrush(Colors.Green);
-            }
-            catch (Exception ex)
-            {
-                elpReport.Fill = new SolidColorBrush(Colors.Red);
-            }
-          
-        }
+      InitializeComponent();
     }
+    
+    private static void SplitPDF(string basePDFPath, string outputRootPath,int pagesPerPerson)
+    {
+      PdfReader reader = new PdfReader(basePDFPath);
+
+      FileInfo file = new FileInfo(basePDFPath);
+      string pdfFileName = file.Name.Substring(0, file.Name.LastIndexOf(".")) + " - ";
+
+      for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber += pagesPerPerson)
+      {
+        string employeeName = ExtractEmployeeNameFromPDFPage(basePDFPath, pageNumber);
+        string newPdfFileName = string.Format(pdfFileName + "{0}", employeeName);
+
+        string outputPath = System.IO.Path.Combine(outputRootPath, employeeName,"ponto");
+
+        CreateAndSaveEmployeePDF(basePDFPath, outputPath, pageNumber, pagesPerPerson, newPdfFileName);
+      }
+    }
+
+    private static string ExtractEmployeeNameFromPDFPage(string path, int pageNumber)
+    {
+      string pdfTxt = GetTextFromPDFPage(path, pageNumber);
+      string name = GetEmployeeNameOnText(pdfTxt);
+      return name;
+    }
+
+    private static string GetTextFromPDFPage(string path, int pageNumber)
+    {
+      string pdfTxt;
+
+      using (PdfReader reader = new PdfReader(path))
+      {
+        pdfTxt = PdfTextExtractor.GetTextFromPage(reader, pageNumber);
+      }
+
+
+      return pdfTxt;
+    }
+
+    private static string GetEmployeeNameOnText(string pdfTxt)
+    {
+      string baseStringLeft = "Funcionário:";
+      string baseStringRight = "Matrícula:";
+      int Start, End;
+
+      if (pdfTxt.Contains(baseStringLeft) && pdfTxt.Contains(baseStringRight))
+      {
+        Start = pdfTxt.IndexOf(baseStringLeft, 0) + baseStringLeft.Length;
+        End = pdfTxt.IndexOf(baseStringRight, Start);
+        return pdfTxt.Substring(Start, End - Start).Trim();
+      }
+      else
+      {
+        return "";
+      }
+    }
+
+    private static void CreateAndSaveEmployeePDF(string pdfFilePath, string outputPath, int startPage, int pagesPerPerson, string pdfFileName)
+    {
+      using (PdfReader reader = new PdfReader(pdfFilePath))
+      {
+        if (!Directory.Exists(outputPath))
+        {
+          Directory.CreateDirectory(outputPath);
+        }
+
+        Document document = new Document();
+        PdfCopy copy = new PdfCopy(document, new FileStream(outputPath + "\\" + pdfFileName + ".pdf", FileMode.Create));
+        document.Open();
+
+
+        for (int i = 0; i < pagesPerPerson; i++)
+        {
+          copy.AddPage(copy.GetImportedPage(reader, startPage + i));
+        }
+
+        document.Close();
+      }
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+      // Create OpenFileDialog 
+      OpenFileDialog dlg = new OpenFileDialog();
+
+      // Set filter for file extension and default file extension 
+      dlg.DefaultExt = ".pdf";
+      dlg.Filter = "PDF Files (*.pdf)|*.pdf";
+
+
+      // Display OpenFileDialog by calling ShowDialog method 
+      DialogResult result = dlg.ShowDialog();
+      string filename = dlg.FileName;
+
+      // Get the selected file name and display in a TextBox 
+      if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(filename))
+      {
+        txtBasePdf.Text = filename;
+      }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      using (var fbd = new FolderBrowserDialog())
+      {
+        DialogResult result = fbd.ShowDialog();
+
+        if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+        {
+          txtOutputPath.Text = fbd.SelectedPath;
+        }
+      }
+
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        int numberOfPage;
+        Int32.TryParse(txtNumberOfPage.Text, out numberOfPage);
+        SplitPDF(txtBasePdf.Text, txtOutputPath.Text, numberOfPage);
+        System.Windows.MessageBox.Show("SUCESS!!!");
+      }
+      catch (Exception ex)
+      {
+        System.Windows.MessageBox.Show("ERROR!!!");
+      }
+    }
+  }
 }
